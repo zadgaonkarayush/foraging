1d73fd8f9eb339a7a8b2530f83d4679199963ea1
AYUSH SUJAY ZADGAONKAR <104816958+zadgaonkarayush@users.noreply.github.com>
2024-11-10 19:59:43 +0530

    Update userRoute.js

--- server/Routes/userRoute.js
+++ server/Routes/userRoute.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import con from '../utils/db.js';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import multer from 'multer';
 import path from 'path';
