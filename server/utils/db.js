3d600987ef974173aa3d9e700b1eeb82e2b7e4bd
Ayush Zadgaonkar <zadgaonkarayush@gmail.com>
2024-11-10 19:33:06 +0530

    Update db.js and add .env file

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,20 +1,42 @@
-import mysql from 'mysql' // Import the promise-based API
+// import mysql from 'mysql' // Import the promise-based API
 
+
+// const con = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'foraging'
+// });
+// con.connect(function(err){
+//     if(err){
+//         console.log("connection Failed")
+//     }
+//     else{
+//         console.log("Connected")
+//     }
+// })
+
+
+// export default con;
+import mysql from 'mysql';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
 
 const con = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'foraging'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
-con.connect(function(err){
-    if(err){
-        console.log("connection Failed")
-    }
-    else{
-        console.log("Connected")
-    }
-})
 
+con.connect(function(err) {
+    if (err) {
+        console.log("Connection Failed");
+    } else {
+        console.log("Connected");
+    }
+});
 
 export default con;
